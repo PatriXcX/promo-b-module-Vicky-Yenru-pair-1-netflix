ad1b1a80eae18a6de7c698d2f8cc8e9f8926af05
PatriXcX <patriciapicogarcia@gmail.com>
2024-09-21 12:44:41 +0200

    updated endpoint stuff in Netflix exercise

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
+
+const fakeMovies = [
+  {
+    id: 1,
+    title: "Wonder Woman",
+    genre: "Action",
+    image:
+      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
+    category: "Superhero",
+    year: 2017,
+    director: "Patty Jenkins",
+  },
+  {
+    id: 2,
+    title: "Inception",
+    genre: "Science Fiction",
+    image:
+      "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+    category: "Thriller",
+    year: 2010,
+    director: "Christopher Nolan",
+  },
+];
+
 const cors = require("cors");
 
 // create and config server
@@ -12,4 +37,27 @@ server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get ('')
+server.get("/movies", (req, res) => {
+  res.send({
+    success: true,
+    movies: fakeMovies,
+  });
+});
+
+//Connexion mysql
+/*const conn= await getConnection ();
+if ( !conn){
+  res.status(500).json({success: false, error:'Error en la conexión'});
+  return
+}*/
+
+//Lanzar query
+
+/*
+const [results] = await conn.query ('SELECT * FROM magas;');
+//DEvolver reslutados como Json
+
+res.json(results);
+//Cerrar connexión
+
+await conn.close();*/
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,17 +1,17 @@
 // login
 
 const getMoviesFromApi = () => {
-  console.log('Se están pidiendo las películas de la app');
+  console.log("Se están pidiendo las películas de la app");
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//localhost:4000/movies')  
-  .then(response => response.json())
-  .then(data => {
-     return data;
-  });
+  return fetch("//localhost:4000/movies")
+    .then((response) => response.json())
+    .then((fakeMovies) => {
+      return fakeMovies;
+    });
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
